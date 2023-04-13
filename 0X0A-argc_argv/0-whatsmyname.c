77f266fc2dec027bb286b35164557c34717858e5
izebhigie <dizebhigie@gmail.com>
2023-04-13 14:23:51 +0100

    first commit

--- 0X0A-argc_argv/0-whatsmyname.c
+++ 0X0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 #include "main.h"
 
 /**
- * main - prints the name of the program
+ * main - prints the name of  program
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char const *argv[])
 {
+	(void)argc;
 	printf("%s\n", *argv);
-
 	return (0);
 }
--- 0X0A-argc_argv/1-args.c
+++ 0X0A-argc_argv/1-args.c
@@ -2,17 +2,15 @@
 #include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
+ * main - prints the number of arguments passed into it
  * @argc: number of arguments
  * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-(void) argv; /*Ignore argv*/
+	(void) argv; /*Ignore argv*/
 	printf("%d\n", argc - 1);
-
 	return (0);
 }
